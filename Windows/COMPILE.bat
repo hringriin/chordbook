8c1c2842fe427fa5d5543a46d2d64d2573353b1b
Joschka Köster <jkoester@tzi.de>
2015-07-13 16:26:25 +0200

    [Umstrukturierung]
    The Windows folder will be resetted like the Linux folder.
    
    Have a look here 771422e0e5130646ce58da34406afbfb76058b95

--- Windows/COMPILE.bat
+++ Windows/COMPILE.bat
@@ -1,18 +1,18 @@
 path = %PATH%;C:\Strawberry\perl\bin\
 
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Setlist.tex"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Chordbook.tex"
 
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\bibtex.exe" "Setlist"
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Setlist.tex"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\bibtex.exe" "Chordbook"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Chordbook.tex"
 
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\makeglossaries.exe" "Setlist"
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Setlist.tex"
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\makeglossaries.exe" "Setlist"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\makeglossaries.exe" "Chordbook"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Chordbook.tex"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\makeglossaries.exe" "Chordbook"
 
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\bibtex.exe" "Setlist"
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Setlist.tex"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\bibtex.exe" "Chordbook"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Chordbook.tex"
 
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Setlist.tex"
-"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Setlist.tex"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Chordbook.tex"
+"C:\Program Files\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" "Chordbook.tex"
 
 "0_AUFRAEUMEN.bat"
